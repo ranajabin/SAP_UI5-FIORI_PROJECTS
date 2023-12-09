d9efd6d513416dc94469de6e5b0e8ab5766e7588
Rana Jabin <55439423+ranajabin@users.noreply.github.com>
2023-12-09 14:57:00 +0530

    Add files via upload

---
+++ simplechatbot/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["chatbot/simplechatbot/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ simplechatbot/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"chatbot/simplechatbot/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
